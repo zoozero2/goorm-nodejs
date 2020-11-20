e033dd705dade4ecae74dc852f39525def1c7dd9
팡팡팡 <asqw2323@gmail.com>
2020-11-20 21:49:13 +0000

    react node 연동

--- react-app/server/server.js
+++ react-app/server/server.js
@@ -2,8 +2,11 @@
 
 const express = require('express');
 const app = express();
-const api = require('./routes/index');
+const api = require('../routes/index');
 
 app.use('/api', api);
 
 app.listen(3001, () => console.log('Node.js Server is running on port 3001...'));
+
+const cors = require('cors');
+app.use(cors());
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,35 @@
+// src/App.js
+
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: null
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api')
+      .then(res => res.json())
+      .then(data => this.setState({title: data.title}));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div>
+            {this.state.title? <h1>{this.state.title}</h1>:<h1>loading...</h1>}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
